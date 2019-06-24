e9572912272ee84782ac2df58e822ea1a70e2ab9
David <bitomule@gmail.com>
2019-06-24 18:34:51 +0200

    Added thread safety using NSRecursiveLock

--- Sources/Classes/DisposeBag.swift
+++ Sources/Classes/DisposeBag.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import class Foundation.NSRecursiveLock
 
 /**
  Bag that disposes added disposables on `deinit`.
@@ -15,6 +16,8 @@ import Combine
  self.existingDisposeBag = DisposeBag()
  */
 public class DisposeBag {
+    private var _lock = NSRecursiveLock()
+
     // state
     fileprivate var _cancellables = [Cancellable]()
     fileprivate var _isDisposed = false
@@ -30,6 +33,7 @@ public class DisposeBag {
     }
 
     private func _insert(_ cancellable: Cancellable) -> Cancellable? {
+        self._lock.lock(); defer { self._lock.unlock() }
         if self._isDisposed {
             return cancellable
         }
@@ -40,6 +44,7 @@ public class DisposeBag {
     }
 
     private func dispose() {
+        self._lock.lock(); defer { self._lock.unlock() }
         let oldCancellables = self._dispose()
 
         for cancellable in oldCancellables {
