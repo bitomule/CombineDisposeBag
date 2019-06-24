541bb018ffaf056b6261c66c99ac05816559c7bd
David <bitomule@gmail.com>
2019-06-24 18:25:42 +0200

    Clean and rename

--- Sources/Classes/DisposeBag.swift
+++ Sources/Classes/DisposeBag.swift
@@ -7,26 +7,65 @@
 
 import Combine
 
+/**
+ Bag that disposes added disposables on `deinit`.
+ This returns ARC (RAII) like resource management to `Combine`.
+ In case contained disposables need to be disposed, just put a different dispose bag
+ or create a new one in its place.
+ self.existingDisposeBag = DisposeBag()
+ */
 public class DisposeBag {
-    var bag: [Cancellable]
+    // state
+    fileprivate var _cancellables = [Cancellable]()
+    fileprivate var _isDisposed = false
 
-    public init() {
-        bag = [Cancellable]()
+    /// Constructs new empty dispose bag.
+    public init() { }
+
+    /// Adds `cancellable` to be disposed when dispose bag is being deinited.
+    ///
+    /// - parameter disposable: Disposable to add.
+    public func insert(_ cancellable: Cancellable) {
+        self._insert(cancellable)?.cancel()
     }
 
-    func add(_ cancellable: Cancellable) {
-        bag.append(cancellable)
+    private func _insert(_ cancellable: Cancellable) -> Cancellable? {
+        if self._isDisposed {
+            return cancellable
+        }
+
+        self._cancellables.append(cancellable)
+
+        return nil
+    }
+
+    private func dispose() {
+        let oldCancellables = self._dispose()
+
+        for cancellable in oldCancellables {
+            cancellable.cancel()
+        }
+    }
+
+    private func _dispose() -> [Cancellable] {
+        let cancellables = self._cancellables
+
+        self._cancellables.removeAll(keepingCapacity: false)
+        self._isDisposed = true
+
+        return cancellables
     }
 
     deinit {
-        bag.forEach { cancellable in
-            cancellable.cancel()
-        }
+        self.dispose()
     }
 }
 
 public extension Cancellable {
+    /// Adds `self` to `bag`
+    ///
+    /// - parameter bag: `DisposeBag` to add `self` to.
     func disposedBy(_ bag: DisposeBag) {
-        bag.add(self)
+        bag.insert(self)
     }
 }
